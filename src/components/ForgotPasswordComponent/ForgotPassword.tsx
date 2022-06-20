e266d1044f28f793bc44cda784f3f17e4838bc5a
Pelumi Ogunleye <zig3377@gmail.com>
2022-06-20 21:10:21 +0100

    created the login and forgot password pages

--- src/components/ForgotPasswordComponent/ForgotPassword.tsx
+++ src/components/ForgotPasswordComponent/ForgotPassword.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
-// import ForgotPasswordCss from 'ForgotPassword.module.css';
+import { Link } from 'react-router-dom';
+import ForgotpasswordCss from './ForgotPassword.module.css';
+import scoreLogo from '../../assets/scoreLogo.svg';
+import bgImage from '../../assets/bgImage.svg';
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  return (
+    <div className={ForgotpasswordCss.body}>
+      <div className={ForgotpasswordCss.details}>
+        <div className={ForgotpasswordCss.logoContainer}>
+          <Link to='/'>
+            <img src={scoreLogo} alt='Score logo' />
+          </Link>
+        </div>
+        <Link to='/login' className={ForgotpasswordCss.backToLoginStyle}>
+          Go back to Login
+        </Link>
+        <h1 className={ForgotpasswordCss.altH1}>Forgot Password?</h1>
+        <p className={ForgotpasswordCss.sendLink}>Send a link to your email to reset your password.</p>
+        <form action=''>
+          <label htmlFor='email'>Email address</label>
+          <input type='email' name='email' id='email' placeholder='Enter email address' required />
+          <input type='submit' className={ForgotpasswordCss.button} value='Send reset link' />
+        </form>
+        <Link to='/ResetPassword'>Reset Password</Link>
+      </div>
+      <div className={ForgotpasswordCss.imageContainer}>
+        <img src={bgImage} alt='A young man typing on a computer' />
+      </div>
+    </div>
+  );
 }
 
 export default ForgotPassword;
--- src/components/LoginComponent/Login.tsx
+++ src/components/LoginComponent/Login.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
-// import LoginCss from 'Login.module.css';
+import { Link } from 'react-router-dom';
+import LoginCss from './Login.module.css';
+import scoreLogo from '../../assets/scoreLogo.svg';
+import bgImage from '../../assets/bgImage.svg';
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={LoginCss.body}>
+      <div className={LoginCss.details}>
+        <div className={LoginCss.logoContainer}>
+          <Link to='/'>
+            <img src={scoreLogo} alt='Score logo' />
+          </Link>
+        </div>
+        <h1>Login to your account</h1>
+        <form action=''>
+          <label htmlFor='email'>Email address</label>
+          <input type='email' name='email' id='email' placeholder='Enter email address' required />
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Enter password'
+            pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'
+            required
+          />
+          <div className={LoginCss.paswordRequirements}>
+            Your password must be at least 6 characters as well as contain at least one uppercase, one lowercase, and
+            one number.
+          </div>
+          <Link to='/ForgotPassword' className={LoginCss.forgotPasswordLink}>
+            Forgot Password?
+          </Link>
+          <input type='submit' className={LoginCss.button} value='Login' />
+        </form>
+      </div>
+      <div className={LoginCss.imageContainer}>
+        <img src={bgImage} alt='A young man typing on a computer' />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
